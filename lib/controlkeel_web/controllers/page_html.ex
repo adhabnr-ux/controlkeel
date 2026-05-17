d8b9b77f9669992ed938e8d92b3dfe513091912a
saragam-git <git@saragam.com.np>
2026-05-17 22:36:29 +0545

    refactor/web-homepage: move format_percent and format_number functions to PageHTML module

--- lib/controlkeel_web/controllers/page_html.ex
+++ lib/controlkeel_web/controllers/page_html.ex
@@ -2,4 +2,12 @@ defmodule ControlKeelWeb.PageHTML do
   use ControlKeelWeb, :html
 
   embed_templates "page_html/*"
+
+  def format_percent(nil), do: "Not recorded"
+  def format_percent(value) when is_float(value), do: "#{Float.round(value, 1)}%"
+  def format_percent(value), do: "#{value}%"
+
+  def format_number(nil), do: "Not recorded"
+  def format_number(value) when is_float(value), do: Float.round(value, 1)
+  def format_number(value), do: value
 end
